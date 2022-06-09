662dce7990c291d013967e1a76aca437832bc1ee
Artur Rybak <arturybak@outlook.com>
2022-06-09 20:44:07 +0100

    Add Different Color Modes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let dimensions = 16;
+let mode = 'shading';
 const genButton = document.querySelector('button');
 genButton.addEventListener('click', function () {
     let input = parseInt(prompt('How many rows/columns?', dimensions));
@@ -8,16 +9,33 @@ genButton.addEventListener('click', function () {
 
 const grid = document.querySelector('.grid');
 
+function colorSquare(square) {
+    switch (mode) {
+        case 'color':
+            square.style.backgroundColor = 'plum';
+            break;
+        case 'rainbow':
+            square.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            break;
+        case 'shading':
+            let currentColor = square.style.backgroundColor;
+            let opacity = Number(currentColor.slice(-4, -1))
+            if (opacity < 1) {
+                square.style.backgroundColor = `rgba(0, 0, 0, ${opacity + 0.1})`;
+            }
+    }
+}
+
 function generateGrid() {
     grid.innerHTML = '';
     grid.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`
-    for(let i = 0; i < dimensions * dimensions; i++) {
+    for (let i = 0; i < dimensions * dimensions; i++) {
         const square = document.createElement('div');
         square.className = 'square';
-        square.addEventListener('mouseover', () => square.classList.add('hovered'));
+        square.addEventListener('mouseover', () => colorSquare(square));
         grid.appendChild(square);
-    }    
+    }
 }
 
 generateGrid();
