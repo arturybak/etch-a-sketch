503a2032a14a5babeb064cf41b548eb577732b5c
Artur Rybak <arturybak@outlook.com>
2022-06-09 19:38:18 +0100

    Set Up Grid

---
+++ app.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector('.grid');
+grid.style.gridTemplateColumns = `repeat(16, 1fr)`
+// grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+grid.style.gridTemplateRows = `repeat(16, 1fr)`
+
+for(let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.textContent = '1';
+    grid.appendChild(square);
+}
