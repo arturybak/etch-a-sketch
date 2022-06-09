60cf1986af06f6de2082495fd09ccc58f7499fed
Artur Rybak <arturybak@outlook.com>
2022-06-09 20:05:35 +0100

    Add Button for Resizing Grid

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const grid = document.querySelector('.grid');
-grid.style.gridTemplateColumns = `repeat(16, 1fr)`
-// grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-grid.style.gridTemplateRows = `repeat(16, 1fr)`
+let dimensions = 16;
+const genButton = document.querySelector('button');
+genButton.addEventListener('click', function () {
+    let input = parseInt(prompt('How many rows/columns?', dimensions));
+    (input > 0 && input < 100) ? dimensions = input : alert('The grid has to be 1-99 rows wide');
+    generateGrid();
+});
 
-for(let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    square.addEventListener('mouseover', () => square.classList.add('hovered'));
-    grid.appendChild(square);
+const grid = document.querySelector('.grid');
+
+function generateGrid() {
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`
+    for(let i = 0; i < dimensions * dimensions; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.addEventListener('mouseover', () => square.classList.add('hovered'));
+        grid.appendChild(square);
+    }    
 }
+
+generateGrid();
+
